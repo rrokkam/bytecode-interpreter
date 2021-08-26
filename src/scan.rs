9f47f21c6e68fd846f8c6a106870d98305ce9a2a
rrokkam <rohithrokkam@gmail.com>
2021-08-25 19:18:34 -0700

    F left paren produces single token

--- src/scan.rs
+++ src/scan.rs
@@ -1,16 +1,46 @@
-pub struct Token {}
+#[rustfmt::skip]
+#[derive(Debug, PartialEq)]
+pub enum TokenKind {
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Semicolon,
+    Minus, Plus, Slash, Star,
+}
 
-pub fn tokenize(_source: &str) -> impl Iterator<Item = Token> {
-    std::iter::empty()
+#[derive(Debug, PartialEq)]
+pub struct Token {
+    kind: TokenKind,
+}
+
+pub fn tokenize(source: &str) -> Box<dyn Iterator<Item = Token>> {
+    let first = source.chars().next().unwrap_or_default();
+    match first {
+        '(' => Box::new(std::iter::once(Token {
+            kind: TokenKind::LeftParen,
+        })),
+        _ => Box::new(std::iter::empty()),
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
     #[test]
     fn empty_source_produces_no_tokens() {
         let source = "";
         let mut tokens = tokenize(source);
         assert!(tokens.next().is_none());
+    }
+
+    #[test]
+    fn left_paren_produces_single_token() {
+        let source = "(";
+        let mut tokens = tokenize(source);
+        assert_eq!(
+            tokens.next().unwrap(),
+            Token {
+                kind: TokenKind::LeftParen
+            }
+        );
     }
 }
