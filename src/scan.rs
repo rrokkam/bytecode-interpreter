2cd97b4c6d81fdcb895b14772f34adb1b3d88eb5
rrokkam <rohithrokkam@gmail.com>
2021-08-27 01:04:25 -0700

    F tokenize and group numbers

--- src/scan.rs
+++ src/scan.rs
@@ -11,6 +11,8 @@ pub enum TokenKind {
     GreaterEqual, Greater,
     LessEqual, Less,
     BangEqual, Bang,
+
+    Number,
 }
 
 #[derive(Debug, PartialEq)]
@@ -25,6 +27,11 @@ pub struct Tokens<'source> {
 impl<'source> Tokens<'source> {
     fn next_matches(&mut self, c: char) -> bool {
         self.chars.next_if_eq(&c).is_some()
+    }
+
+    fn number(&mut self, _: char) -> TokenKind {
+        while self.chars.next_if(char::is_ascii_digit).is_some() {}
+        TokenKind::Number
     }
 }
 
@@ -56,6 +63,7 @@ impl<'source> Iterator for Tokens<'source> {
                     '<' => Less,
                     '!' if self.next_matches('=') => BangEqual,
                     '!' => Bang,
+                    c if c.is_ascii_digit() => self.number(c),
                     _ => todo!(),
                 },
             })
@@ -123,6 +131,18 @@ mod test {
         assert_eq!(tokens.next().unwrap(), Token { kind: GreaterEqual });
         assert_eq!(tokens.next().unwrap(), Token { kind: Equal });
         assert_eq!(tokens.next().unwrap(), Token { kind: LessEqual });
+        assert!(tokens.next().is_none());
+    }
+
+    #[test]
+    fn numbers_are_grouped() {
+        use TokenKind::*;
+        let source = "123 (534)";
+        let mut tokens = tokenize(source);
+        assert_eq!(tokens.next().unwrap(), Token { kind: Number });
+        assert_eq!(tokens.next().unwrap(), Token { kind: LeftParen });
+        assert_eq!(tokens.next().unwrap(), Token { kind: Number });
+        assert_eq!(tokens.next().unwrap(), Token { kind: RightParen });
         assert!(tokens.next().is_none());
     }
 }
