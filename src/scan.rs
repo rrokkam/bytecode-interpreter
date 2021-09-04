ead0dd437cfe89e87a56c2352f5491b74a6cf12f
rrokkam <rohithrokkam@gmail.com>
2021-09-04 15:14:44 -0700

    F Tokens store only start positions

--- src/scan.rs
+++ src/scan.rs
@@ -18,18 +18,17 @@ enum Kind {
 #[derive(Debug, PartialEq)]
 pub struct Token {
     kind: Kind,
-    indices: std::ops::RangeInclusive<usize>,
+    position: usize,
 }
 
 impl Token {
-    fn new(kind: Kind, indices: std::ops::RangeInclusive<usize>) -> Token {
-        Token { kind, indices }
+    fn new(kind: Kind, position: usize) -> Token {
+        Token { kind, position }
     }
 }
 
 pub struct Tokens<'source> {
     char_indices: std::iter::Peekable<std::str::CharIndices<'source>>,
-    end_index: usize,
 }
 
 impl<'source> Tokens<'source> {
@@ -61,11 +60,9 @@ impl<'source> Tokens<'source> {
     }
 
     fn next_matches(&mut self, expected: char) -> bool {
-        let next = self.char_indices.next_if(|actual| actual.1 == expected);
-        if let Some((end, _)) = next {
-            self.end_index = end;
-        }
-        next.is_some()
+        self.char_indices
+            .next_if(|actual| actual.1 == expected)
+            .is_some()
     }
 
     fn number(&mut self, _: char) -> Kind {
@@ -73,9 +70,7 @@ impl<'source> Tokens<'source> {
             .char_indices
             .next_if(|(_, c)| c.is_ascii_digit())
             .is_some()
-        {
-            self.end_index += 1
-        }
+        {}
         Kind::Number
     }
 }
@@ -84,25 +79,15 @@ impl<'source> Iterator for Tokens<'source> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Token> {
-        let token = self
-            .char_indices
-            .find(|c| !c.1.is_ascii_whitespace())
-            .map(|c| {
-                Token::new(
-                    self.kind(c.1),
-                    // override the very first character's end
-                    c.0..=std::cmp::max(c.0, self.end_index),
-                )
-            })?;
-        self.end_index = *token.indices.end() + 1;
-        Some(token)
+        self.char_indices
+            .find(|(_, c)| !c.is_ascii_whitespace())
+            .map(|(i, c)| Token::new(self.kind(c), i))
     }
 }
 
 pub fn tokenize(source: &str) -> impl Iterator<Item = Token> + '_ {
     Tokens {
         char_indices: source.char_indices().peekable(),
-        end_index: 0,
     }
 }
 
@@ -123,17 +108,17 @@ mod test {
         let source = "(),.;{}/-*+";
         let mut tokens = tokenize(source);
 
-        assert_eq!(tokens.next().unwrap(), Token::new(LeftParen, 0..=0));
-        assert_eq!(tokens.next().unwrap(), Token::new(RightParen, 1..=1));
-        assert_eq!(tokens.next().unwrap(), Token::new(Comma, 2..=2));
-        assert_eq!(tokens.next().unwrap(), Token::new(Dot, 3..=3));
-        assert_eq!(tokens.next().unwrap(), Token::new(Semicolon, 4..=4));
-        assert_eq!(tokens.next().unwrap(), Token::new(LeftBrace, 5..=5));
-        assert_eq!(tokens.next().unwrap(), Token::new(RightBrace, 6..=6));
-        assert_eq!(tokens.next().unwrap(), Token::new(Slash, 7..=7));
-        assert_eq!(tokens.next().unwrap(), Token::new(Minus, 8..=8));
-        assert_eq!(tokens.next().unwrap(), Token::new(Star, 9..=9));
-        assert_eq!(tokens.next().unwrap(), Token::new(Plus, 10..=10));
+        assert_eq!(tokens.next().unwrap(), Token::new(LeftParen, 0));
+        assert_eq!(tokens.next().unwrap(), Token::new(RightParen, 1));
+        assert_eq!(tokens.next().unwrap(), Token::new(Comma, 2));
+        assert_eq!(tokens.next().unwrap(), Token::new(Dot, 3));
+        assert_eq!(tokens.next().unwrap(), Token::new(Semicolon, 4));
+        assert_eq!(tokens.next().unwrap(), Token::new(LeftBrace, 5));
+        assert_eq!(tokens.next().unwrap(), Token::new(RightBrace, 6));
+        assert_eq!(tokens.next().unwrap(), Token::new(Slash, 7));
+        assert_eq!(tokens.next().unwrap(), Token::new(Minus, 8));
+        assert_eq!(tokens.next().unwrap(), Token::new(Star, 9));
+        assert_eq!(tokens.next().unwrap(), Token::new(Plus, 10));
         assert!(tokens.next().is_none());
     }
 
@@ -142,10 +127,10 @@ mod test {
         use Kind::*;
         let source = " ( ) .\n  *";
         let mut tokens = tokenize(source);
-        assert_eq!(tokens.next().unwrap(), Token::new(LeftParen, 1..=1));
-        assert_eq!(tokens.next().unwrap(), Token::new(RightParen, 3..=3));
-        assert_eq!(tokens.next().unwrap(), Token::new(Dot, 5..=5));
-        assert_eq!(tokens.next().unwrap(), Token::new(Star, 9..=9));
+        assert_eq!(tokens.next().unwrap(), Token::new(LeftParen, 1));
+        assert_eq!(tokens.next().unwrap(), Token::new(RightParen, 3));
+        assert_eq!(tokens.next().unwrap(), Token::new(Dot, 5));
+        assert_eq!(tokens.next().unwrap(), Token::new(Star, 9));
         assert!(tokens.next().is_none());
     }
 
@@ -154,15 +139,15 @@ mod test {
         use Kind::*;
         let source = "===!!=<>==<=>";
         let mut tokens = tokenize(source);
-        assert_eq!(tokens.next().unwrap(), Token::new(EqualEqual, 0..=1));
-        assert_eq!(tokens.next().unwrap(), Token::new(Equal, 2..=2));
-        assert_eq!(tokens.next().unwrap(), Token::new(Bang, 3..=3));
-        assert_eq!(tokens.next().unwrap(), Token::new(BangEqual, 4..=5));
-        assert_eq!(tokens.next().unwrap(), Token::new(Less, 6..=6));
-        assert_eq!(tokens.next().unwrap(), Token::new(GreaterEqual, 7..=8));
-        assert_eq!(tokens.next().unwrap(), Token::new(Equal, 9..=9));
-        assert_eq!(tokens.next().unwrap(), Token::new(LessEqual, 10..=11));
-        assert_eq!(tokens.next().unwrap(), Token::new(Greater, 12..=12));
+        assert_eq!(tokens.next().unwrap(), Token::new(EqualEqual, 0));
+        assert_eq!(tokens.next().unwrap(), Token::new(Equal, 2));
+        assert_eq!(tokens.next().unwrap(), Token::new(Bang, 3));
+        assert_eq!(tokens.next().unwrap(), Token::new(BangEqual, 4));
+        assert_eq!(tokens.next().unwrap(), Token::new(Less, 6));
+        assert_eq!(tokens.next().unwrap(), Token::new(GreaterEqual, 7));
+        assert_eq!(tokens.next().unwrap(), Token::new(Equal, 9));
+        assert_eq!(tokens.next().unwrap(), Token::new(LessEqual, 10));
+        assert_eq!(tokens.next().unwrap(), Token::new(Greater, 12));
         assert!(tokens.next().is_none());
     }
 
@@ -171,10 +156,10 @@ mod test {
         use Kind::*;
         let source = "123 (534)";
         let mut tokens = tokenize(source);
-        assert_eq!(tokens.next().unwrap(), Token::new(Number, 0..=2));
-        assert_eq!(tokens.next().unwrap(), Token::new(LeftParen, 4..=4));
-        assert_eq!(tokens.next().unwrap(), Token::new(Number, 5..=7));
-        assert_eq!(tokens.next().unwrap(), Token::new(RightParen, 8..=8));
+        assert_eq!(tokens.next().unwrap(), Token::new(Number, 0));
+        assert_eq!(tokens.next().unwrap(), Token::new(LeftParen, 4));
+        assert_eq!(tokens.next().unwrap(), Token::new(Number, 5));
+        assert_eq!(tokens.next().unwrap(), Token::new(RightParen, 8));
         assert!(tokens.next().is_none());
     }
 }
