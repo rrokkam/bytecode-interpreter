0765eece8161336fbab2b92613688f7f1a6787de
rrokkam <rohithrokkam@gmail.com>
2021-09-05 13:25:44 -0700

    F tokenize keywords

--- src/scan.rs
+++ src/scan.rs
@@ -12,7 +12,18 @@ enum Kind {
     LessEqual, Less,
     BangEqual, Bang,
 
+    // Literals
     Number, String, Identifier,
+
+    // Boolean logic-related keywords
+    And, Or, True, False,
+    If, Else, For, While,
+
+    // Object-related keywords
+    Class, Nil, Super, This, Var,
+
+    // Function-related keywords
+    Function, Print, Return,
 
     Comment,
 
@@ -61,7 +72,7 @@ impl<'source> Tokens<'source> {
             '!' => Bang,
             '"' => self.string(),
             c if c.is_numeric() => self.number(),
-            c if c.is_alphabetic() => self.identifier(),
+            c if c.is_alphabetic() => self.identifier_or_keyword(c),
             _ => Error,
         }
     }
@@ -80,10 +91,57 @@ impl<'source> Tokens<'source> {
         {}
     }
 
+    fn identifier_or_keyword(&mut self, current: char) -> Kind {
+        use Kind::*;
+        match current {
+            'a' => self.try_keyword("nd", And),
+            'c' => self.try_keyword("lass", Class),
+            'e' => self.try_keyword("lse", Else),
+            'f' => match self.char_indices.peek() {
+                Some((_, 'a')) => self.try_keyword("alse", False),
+                Some((_, 'o')) => self.try_keyword("or", For),
+                Some((_, 'u')) => self.try_keyword("unction", Function),
+                _ => self.identifier(),
+            },
+            'i' => self.try_keyword("f", If),
+            'n' => self.try_keyword("il", Nil),
+            'o' => self.try_keyword("r", Or),
+            'p' => self.try_keyword("rint", Print),
+            'r' => self.try_keyword("eturn", Return),
+            's' => self.try_keyword("uper", Super),
+            't' => match self.char_indices.peek() {
+                Some((_, 'h')) => self.try_keyword("his", This),
+                Some((_, 'r')) => self.try_keyword("rue", True),
+                _ => self.identifier(),
+            },
+            'v' => self.try_keyword("ar", Var),
+            'w' => self.try_keyword("hile", While),
+            _ => self.identifier(),
+        }
+    }
+
+    fn try_keyword(&mut self, keyword: &str, keyword_kind: Kind) -> Kind {
+        let identifier_starts_with_keyword = keyword
+            .chars()
+            .find(|&expected| !self.next_matches(expected))
+            .is_none();
+        let keyword_is_not_prefix_of_identifier = self
+            .char_indices
+            .peek()
+            .filter(|actual| actual.1.is_alphanumeric())
+            .is_none();
+
+        if identifier_starts_with_keyword && keyword_is_not_prefix_of_identifier {
+            keyword_kind
+        } else {
+            self.identifier()
+        }
+    }
+
     fn identifier(&mut self) -> Kind {
         while self
             .char_indices
-            .next_if(|(_, c)| c.is_alphabetic())
+            .next_if(|(_, c)| c.is_alphanumeric())
             .is_some()
         {}
         Kind::Identifier
@@ -238,6 +296,42 @@ mod test {
         assert_eq!(tokens.next().unwrap(), Token::new(10, Identifier));
         assert_eq!(tokens.next().unwrap(), Token::new(14, Identifier));
         assert_eq!(tokens.next().unwrap(), Token::new(25, Semicolon));
+        assert!(tokens.next().is_none());
+    }
+
+    #[test]
+    fn keywords() {
+        let source =
+            "and or true false if else for while class nil super this var function print return";
+        let mut tokens = tokenize(source);
+        assert_eq!(tokens.next().unwrap(), Token::new(0, And));
+        assert_eq!(tokens.next().unwrap(), Token::new(4, Or));
+        assert_eq!(tokens.next().unwrap(), Token::new(7, True));
+        assert_eq!(tokens.next().unwrap(), Token::new(12, False));
+        assert_eq!(tokens.next().unwrap(), Token::new(18, If));
+        assert_eq!(tokens.next().unwrap(), Token::new(21, Else));
+        assert_eq!(tokens.next().unwrap(), Token::new(26, For));
+        assert_eq!(tokens.next().unwrap(), Token::new(30, While));
+
+        assert_eq!(tokens.next().unwrap(), Token::new(36, Class));
+        assert_eq!(tokens.next().unwrap(), Token::new(42, Nil));
+        assert_eq!(tokens.next().unwrap(), Token::new(46, Super));
+        assert_eq!(tokens.next().unwrap(), Token::new(52, This));
+        assert_eq!(tokens.next().unwrap(), Token::new(57, Var));
+
+        assert_eq!(tokens.next().unwrap(), Token::new(61, Function));
+        assert_eq!(tokens.next().unwrap(), Token::new(70, Print));
+        assert_eq!(tokens.next().unwrap(), Token::new(76, Return));
+        assert!(tokens.next().is_none());
+    }
+
+    #[test]
+    fn identifier_or_keyword() {
+        let source = "fidentifier tidentifier uidentifier";
+        let mut tokens = tokenize(source);
+        assert_eq!(tokens.next().unwrap(), Token::new(0, Identifier));
+        assert_eq!(tokens.next().unwrap(), Token::new(12, Identifier));
+        assert_eq!(tokens.next().unwrap(), Token::new(24, Identifier));
         assert!(tokens.next().is_none());
     }
 }
